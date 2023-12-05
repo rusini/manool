deeeaeb1994d644c8dd340bdef35385c360c9da1
rusini <rusini@manool.org>
2023-12-04 21:05:58 -0500

    Backup

--- base-opt-apply2.cc
+++ base-opt-apply2.cc
@@ -116,7 +116,7 @@ auto mnl::aux::optimize(expr_apply<2> expr)->code {
          case sym::id("=="): return optimize(op<sym::id("==")>);
          case sym::id("<>"): return optimize(op<sym::id("<>")>);
          }
-      } // if (auto target_p = as_p<expr_lit<const sym &>>(expr.target))
+      }
       // all numeric, except where belongs to (*)
       {  const auto optimize = [&](auto op) MNL_INLINE->code{
             if (auto arg0_p = as_p<expr_lit<long long>>(expr.arg0)) [[unlikely]] {
@@ -312,7 +312,7 @@ auto mnl::aux::optimize(expr_apply<2> expr)->code {
          };
          switch (target_p->value) {
          case sym::id("&"): return optimize(op<sym::id("&")>);
-         case sym::id("|"): return optimize(op<sym::id("|" )>);
+         case sym::id("|"): return optimize(op<sym::id("|")>);
          }
       }
       // assuming a binary operation with no observable side effects (*) // talking side effects because there's no opt for lit,lit, etc. assuming the operation returns, not producing side effects, and thus lit,lit should be treated as constant folding!!!
@@ -396,7 +396,7 @@ auto mnl::aux::optimize(expr_apply<2> expr)->code {
       if (auto arg1_p = as_p<expr_tvar>(expr.arg1)) [[unlikely]]
          return expr_apply{{}, *target_p, std::move(expr.arg0), *arg1_p, std::move(expr._loc)}; // ?Ls[?; ?T]
       return expr_apply{{}, *target_p, std::move(expr.arg0), std::move(expr.arg1), std::move(expr._loc)}; // ?Ls[?; ?]
-   }
+   } // if (auto target_p = as_p<expr_lit<const sym &>>(expr.target))
    // assuming indexing into fast containers
    if (auto target_p = as_p<expr_lit<>>(expr.target)) [[unlikely]] {
       if (auto arg0_p = as_p<expr_tvar>(expr.arg0)) [[unlikely]] {
