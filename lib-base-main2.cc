b54aa4da13358c1feaae061ddb7b84272b199f96
rusini <rusini@manool.org>
2025-01-08 17:52:14 -0500

    Backup

--- lib-base-main2.cc
+++ lib-base-main2.cc
@@ -438,39 +438,39 @@ namespace aux { namespace {
    template<typename Arg_count> template<typename Self, typename Arg0>
    MNL_INLINE val box<_expr_proc<Arg_count>>::apply(Self &&self, Arg0 &&arg0) {
       stk_check();
-      if (MNL_UNLIKELY(1 != arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
-      int index;
-      return tstack.frame_guard(), tstack.scope_guard(index), [&](decltype(tstack) &MNL_RESTRICT tstack = tstack)
-         MNL_INLINE{ tstack.push(std::forward<Arg0>(arg0)), ++index; }(),
-         body.execute();
+      if (MNL_UNLIKELY(1 != dat.arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
+      int index = 0;
+      return tstack.frame_guard(), tstack.scope_guard(index), [&] MNL_INLINE(decltype(tstack) &MNL_RESTRICT tstack = tstack)
+         { tstack.push(std::forward<Arg0>(arg0)), ++index; }(),
+         dat.body.execute();
    }
    template<typename Arg_count> template<typename Self, typename Arg0, typename Arg1>
    MNL_INLINE val box<_expr_proc<Arg_count>>::apply(Self &&self, Arg0 &&arg0, Arg1 &&arg1) {
       stk_check();
-      if (MNL_UNLIKELY(2 != arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
-      int index;
-      return tstack.frame_guard(), tstack.scope_guard(index), [&](decltype(tstack) &MNL_RESTRICT tstack = tstack)
-         MNL_INLINE{ tstack.push(std::forward<Arg0>(arg0)), ++index; tstack.push(std::forward<Arg1>(arg1)), ++index; }(),
-         body.execute();
+      if (MNL_UNLIKELY(2 != dat.arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
+      int index = 0;
+      return tstack.frame_guard(), tstack.scope_guard(index), [&] MNL_INLINE(decltype(tstack) &MNL_RESTRICT tstack = tstack)
+         { tstack.push(std::forward<Arg0>(arg0)), ++index; tstack.push(std::forward<Arg1>(arg1)), ++index; }(),
+         dat.body.execute();
    }
    template<typename Arg_count> template<typename Self>
    MNL_INLINE val box<_expr_proc<Arg_count>>::apply(Self &&self, int argc, val argv[]) {
       stk_check();
-      if (MNL_UNLIKELY(argc != arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
+      if (MNL_UNLIKELY(argc != dat.arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
       int index;
-      return tstack.frame_guard(), tstack.scope_guard(index), [&](decltype(tstack) &MNL_RESTRICT tstack = tstack, int count = dat.arg_count)
-         MNL_INLINE{ MNL_UNROLL(10) for (index = 0; index < count; ++index) tstack.push_back(std::move(argv[index])); }(),
-         body.execute();
+      return tstack.frame_guard(), tstack.scope_guard(index), [&] MNL_INLINE(decltype(tstack) &MNL_RESTRICT tstack = tstack)
+         { MNL_UNROLL(10) for (index = 0; index < argc; ++index) tstack.push_back(std::move(argv[index])); }(),
+         dat.body.execute();
    }
    template<typename Arg_count> template<typename Self>
    MNL_INLINE val box<_expr_proc<Arg_count>>::invoke(Self &&self, const sym &op, int argc, val argv[], val *) {
       stk_check();
       if (MNL_UNLIKELY(op != MNL_SYM("Apply"))) return self.default_invoke(op, argc, argv);
-      if (MNL_UNLIKELY(argc != arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
+      if (MNL_UNLIKELY(argc != dat.arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
       int index;
-      return tstack.frame_guard(), tstack.scope_guard(index), [&](decltype(tstack) &MNL_RESTRICT tstack = tstack, int count = dat.arg_count)
-         MNL_INLINE{ MNL_UNROLL(10) for (index = 0; index < count; ++index) tstack.push_back(std::move(argv[index])); }(),
-         body.execute();
+      return tstack.frame_guard(), tstack.scope_guard(index), [&] MNL_INLINE(decltype(tstack) &MNL_RESTRICT tstack = tstack)
+         { MNL_UNROLL(10) for (index = 0; index < argc; ++index) tstack.push_back(std::move(argv[index])); }(),
+         dat.body.execute();
    }
 
    class comp_proc { MNL_NONVALUE()
