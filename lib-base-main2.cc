eb29ef48b98e9cb95cdc6660b6fd0abc2ed2f486
rusini <rusini@manool.org>
2025-01-16 11:28:29 -0500

    Backup

--- lib-base-main2.cc
+++ lib-base-main2.cc
@@ -438,24 +438,24 @@ namespace aux { namespace {
    template<typename Arg_count> template<typename Self, typename Arg0>
    MNL_INLINE val box<_expr_proc<Arg_count>>::apply(Self &&self, Arg0 &&arg0) {
       if (MNL_UNLIKELY(1 != dat.arg_count)) return default_apply(std::forward<Self>(self), std::forward<Arg0>(arg0));
-      stk_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
-      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(decltype(vstack) &MNL_RESTRICT vstack = vstack)
+      stack_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
+      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(class vstack &MNL_RESTRICT vstack = vstack)
          { vstack.push(std::forward<Arg0>(arg0)); }(),
          dat.body.execute();
    }
    template<typename Arg_count> template<typename Self, typename Arg0, typename Arg1>
    MNL_INLINE val box<_expr_proc<Arg_count>>::apply(Self &&self, Arg0 &&arg0, Arg1 &&arg1) {
-      if (MNL_UNLIKELY(2 != dat.arg_count)) return default_apply(std::forward<Self>(self), std::forward<Key0>(arg0), std::forward<Arg1>(arg1));
-      stk_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
-      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(decltype(vstack) &MNL_RESTRICT vstack = vstack)
+      if (MNL_UNLIKELY(2 != dat.arg_count)) return default_apply(std::forward<Self>(self), std::forward<Arg0>(arg0), std::forward<Arg1>(arg1));
+      stack_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
+      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(class vstack &MNL_RESTRICT vstack = vstack)
          { vstack.push(std::forward<Arg0>(arg0)); vstack.push(std::forward<Arg1>(arg1)); }(),
          dat.body.execute();
    }
    template<typename Arg_count> template<typename Self>
    MNL_INLINE val box<_expr_proc<Arg_count>>::apply(Self &&self, int argc, val argv[]) {
       if (MNL_UNLIKELY(argc != dat.arg_count)) return default_apply(std::forward<Self>(self), argc, argv);
-      stk_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
-      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(decltype(vstack) &MNL_RESTRICT vstack = vstack)
+      stack_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
+      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(class vstack &MNL_RESTRICT vstack = vstack)
          { MNL_UNROLL(10) while (argc--) vstack.push(std::move(*argv++)); }(),
          dat.body.execute();
    }
@@ -463,8 +463,8 @@ namespace aux { namespace {
    MNL_INLINE val box<_expr_proc<Arg_count>>::invoke(Self &&self, const sym &op, int argc, val argv[], val *) {
       if (MNL_UNLIKELY(op != MNL_SYM("Apply"))) return self.default_invoke(op, argc, argv);
       if (MNL_UNLIKELY(argc != dat.arg_count)) MNL_ERR(MNL_SYM("InvalidInvocation"));
-      stk_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
-      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(decltype(vstack) &MNL_RESTRICT vstack = vstack)
+      stack_check(sizeof(vstack::storage_elem [dat.var_count])); asm("" : "+rm" (dat.var_count)); vstack::storage_elem frame[dat.var_count];
+      return vstack.frame_guard(frame), vstack.scope_guard(), [&] MNL_INLINE(class vstack &MNL_RESTRICT vstack = vstack)
          { MNL_UNROLL(10) while (argc--) vstack.push(std::move(*argv++)); }(),
          dat.body.execute();
    }
